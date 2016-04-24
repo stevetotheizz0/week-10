8f30cac0dfe59aadb3280c56e6a8cab48954c9bc
stevetotheizz0 <stephenjskilton@gmail.com>
2016-04-24 16:06:38 -0400

    Done and Done

--- assignment/js/setup.js
+++ assignment/js/setup.js
@@ -15,9 +15,38 @@ var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z
 // The viz.json output by publishing on cartodb
 var layerUrl = 'https://stephenjskilton.cartodb.com/api/v2/viz/c948b850-ef27-11e5-a54c-0e5db1731f59/viz.json';
 
-cartodb.createLayer(map, layerUrl).addTo(map).on('done', function(layer){
-    //I'm just messing around here with the getSubLayer and setSQL calls
-    testLayer = layer.getSubLayer(1);
-    testLayer.setSQL( "SELECT * FROM nyc_flickr_photos_merge WHERE date >= '1900-01-01 00:00:00' and date < '2000-12-31 00:00:00' ");
+var drawControl = new L.Control.Draw({
+  draw: {
+    polyline: false,
+    polygon: false,
+    circle: false,
+    marker: true,
+    rectangle: true,
+  }
+});
 
+var drawnItems = new L.FeatureGroup();
+
+var drawnLayerID;
+
+map.on('draw:created', function (e) {
+  var type = e.layerType;
+  var layer = e.layer;
+
+  if (type === 'marker') {
+    nClosest(layer._latlng, 5);
+  } else if (type === 'rectangle') {
+    pointsWithin(layer._latlngs);
+  }
+
+  if (drawnLayerID) { map.removeLayer(map._layers[drawnLayerID]); }
+  map.addLayer(layer);
+  drawnLayerID = layer._leaflet_id;
+});
+
+
+cartodb.createLayer(map, layerUrl).addTo(map).on('done', function(layer){
+    testLayer = layer;
+    map.addControl(drawControl);
+    map.addLayer(drawnItems);
 });
